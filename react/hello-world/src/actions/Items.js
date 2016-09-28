022179e8b62e5be60c0f9802714e12fea811980d
Dustin Higginbotham <dustinh052@gmail.com>
2016-09-28 13:06:02 -0600

    readme and comments

---
+++ react/hello-world/src/actions/Items.js
@@ -0,0 +1,3 @@
+// Your action should be defined in here. Every action should be a function that
+// only returns a plain object with a "type" property defined. Use constants to help
+// pass this data along.
