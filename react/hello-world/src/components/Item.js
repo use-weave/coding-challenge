ac27a90ba776d8db06f5c99e41ab1109cda4e1cd
Kevin Ridge <mrrydge91@gmail.com>
2016-10-10 08:45:07 -0400

    kevins submission

--- react/hello-world/src/components/Item.js
+++ react/hello-world/src/components/Item.js
@@ -1,1 +1,28 @@
 import React, { Component } from 'react'
+class Item extends React.Component{
+  render(){
+    return(
+      <Form addItem={this._addItem.bind(this)}/>
+      <p className="itemname">{this.props.name}</p>
+      );
+  }
+
+  constructor(){
+    super();
+
+    this.state = {
+      showItems: false
+      Items: []
+    }
+  }
+
+  _addItem(name){
+    const item = {
+      id=this.state.item.length+1,
+      name
+    };
+    this.setState({items: this.state.items.concat([item])});
+  }
+
+
+}
