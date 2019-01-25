e65879312c8c13e89f32b8f50b8d3f77a5c67125
keeperofkey <46409345+keeperofkey@users.noreply.github.com>
2019-01-25 11:53:13 -0500

    Update Items.js

--- react/hello-world/src/reducers/Items.js
+++ react/hello-world/src/reducers/Items.js
@@ -1,15 +1,20 @@
 // We load in ItemActions here to be able to reference the constants you hopefully set up.
 import * as ItemActions from 'actions/Items'
 
+
 // Every reducer needs to have something return, whether or not a case matches.
 // A reducer should never return null or undefined.
 function Items(state = [], action) {
-
     switch (action.type) {
-
+        case 'ADD_ITEM':
+            return {
+                id: ItemActions.id,
+                text: ItemActions.text,
+            }
         default:
             return state
     }
 }
+
 
 export default Items
