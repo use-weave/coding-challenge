a5028edeead36f296773a952fc8662651e10284a
keeperofkey <46409345+keeperofkey@users.noreply.github.com>
2019-01-25 11:51:20 -0500

    Update Form.js

--- react/hello-world/src/components/Form.js
+++ react/hello-world/src/components/Form.js
@@ -1,1 +1,32 @@
 import React, { Component } from 'react'
+export default class Form extends Component {
+        constructor(props) {
+          super(props);
+          this.state = {value: ''};
+      
+          this.handleChange = this.handleChange.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
+        }
+      
+        handleChange(event) {
+          this.setState({value: event.target.value});
+        }
+      
+        handleSubmit(event) {
+          alert('A Item was submitted: ' + this.state.value);
+          event.preventDefault();
+        }
+      
+        render() {
+          return (
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Item:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          );
+        }
+}
+
