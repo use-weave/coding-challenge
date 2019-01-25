359b8208815973265133d7f083c8e56ac7d6f548
keeperofkey <46409345+keeperofkey@users.noreply.github.com>
2019-01-25 11:52:30 -0500

    Update Item.js

--- react/hello-world/src/components/Item.js
+++ react/hello-world/src/components/Item.js
@@ -1,1 +1,7 @@
 import React, { Component } from 'react'
+import Items from '../reducers/Items'
+    const reactElementsArray = Items.map((Items) => {
+        return (
+            <p>{Items}</p>
+        );
+    });
