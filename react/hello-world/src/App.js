4b6c1b3f4a81f988f4c42db2a66a0e2a6ddf8f06
keeperofkey <46409345+keeperofkey@users.noreply.github.com>
2019-01-25 11:49:00 -0500

    Update App.js

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -16,7 +16,26 @@ import Form from 'components/Form'
 // Provides a basic connection to redux. If you want to read from the state of
 // the store, you will need to pass in an argument to this function. Examples should
 // be online for this.
-@connect()
+const reactElementsArray = Item.map((data) => {
+    return (
+      console.log(data)
+    );
+  });
+  const reactComponent = props => {
+    return(
+      <div>{reactElementsArray}</div>  
+    );
+  };
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSubmit: (text) => {
+            dispatch(ItemActions(text))
+        }
+    }
+}
+
+
+let AddItem = connect(null, mapDispatchToProps)(Form)
 class App extends Component {
 
     // All components need a render method, and this is the top level of our application's render.
@@ -28,7 +47,7 @@ class App extends Component {
     render() {
         return (
             <div className="container">
-                <h1>What's up from Weave</h1>
+                <Form />
 
                 {/*your code goes here... also this is an example of a comment in JSX*/}
             </div>
