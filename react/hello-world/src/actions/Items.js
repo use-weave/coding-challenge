0564d5d4e685ef8bc81e917094e1bbb67ef9f1b9
keeperofkey <46409345+keeperofkey@users.noreply.github.com>
2019-01-31 16:51:16 -0500

    Add files via upload

--- react/hello-world/src/actions/Items.js
+++ react/hello-world/src/actions/Items.js
@@ -1,11 +1,17 @@
 // Your action should be defined in here. Every action should be a function that
 // only returns a plain object with a "type" property defined. Use constants to help
 // pass this data along.
-let itemId = 0
-export const addItem = (text) => {
+
+export const ADD = "ADD"
+
+export const addItem = item => {
   return {
-    type: 'ADD_ITEM',
-    id: itemId++,
-    text
+    type: ADD,
+    item: item,
+    id: null
   }
 }
+
+
+
+
