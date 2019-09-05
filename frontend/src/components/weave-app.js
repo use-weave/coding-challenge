7e1074e7a50f0da05fc40c80f4a1afabfaf93f4e
Clark <rclark010@gmail.com>
2019-09-05 00:08:21 -0600

    Adds style variables and renames imports

--- frontend/src/components/weave-app.js
+++ frontend/src/components/weave-app.js
@@ -24,7 +24,25 @@ window.customElements.define('weave-app', class extends connect(store)(LitElemen
             SharedStyles,
             css`
                 :host {
-                    --primary-color: var(--weave-primary-color, #007A71);
+                    --primary-color: #0287C3;
+                    --secondary-color: #0276aa;
+                    --text-color: #4b5255;
+                    --text-color-muted: #869094;
+                    --placeholder-color: #aaa;
+                    --background-color: white;
+
+                    --flower-color: #2B6491;
+                    --edible-color: #6C71C1;
+                    --concentrate-color: #396DAC;
+                    --topical-color: #8D63B4;
+                    --merchandise-color: #A7529F;
+
+                    --action-color: #007A71;
+
+                    --exit-red: #CE4047;
+
+                    --box-shadow: rgba(0, 0, 0, 0.22) 0px 1px 6px, rgba(0, 0, 0, 0.22) 0px 1px 4px;
+                    --box-shadow-hover: rgba(0, 0, 0, 0.20) 0px 3px 10px, rgba(0, 0, 0, 0.28) 0px 3px 10px;
                 }
 
                 header {
@@ -59,11 +77,11 @@ window.customElements.define('weave-app', class extends connect(store)(LitElemen
         return html`
 
             <header>
-                <h1>Weave!</h1>
+                <h1>Weave</h1>
             </header>
 
             <main>
-                <weave-driver-assignments class="page" ?active="${this._page == 'home'}"></weave-driver-assignments>
+                <weave-assign-orders class="page" ?active="${this._page == 'home'}"></weave-assign-orders>
                 <weave-error404 class="page" ?active="${this._page == 'error404'}"></weave-error404>
             </main>
 
