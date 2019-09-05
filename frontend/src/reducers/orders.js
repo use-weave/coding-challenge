75b5831c09b5c80570c7c5f4c1c6f242c7a814f0
Clark <rclark010@gmail.com>
2019-09-05 12:53:19 -0600

    Adds status update functionality

--- frontend/src/reducers/orders.js
+++ frontend/src/reducers/orders.js
@@ -1,6 +1,7 @@
+import { STATUS_QUEUE } from '../actions/orders.js';
 
-const INITIAL_STATE = [
-    {
+const INITIAL_STATE = {
+    1290312938: {
         id: '1290312938',
         customer: 'Danny DeVito',
         items: 5,
@@ -10,36 +11,43 @@ const INITIAL_STATE = [
         address_city: 'Boulder',
         address_state: 'CO',
         address_zip: '80303',
-        // created, filled, queued, out for delivery, delivered, cancelled
+        /* Status Options (stubbed):
+         * created, filled, queued, out for delivery, delivered, cancelled
+         */
         statusList: [
             {
-                actor: 'Danny DeVito',
+                actor_id: '1290312938',
+                actor_name: 'Danny DeVito',
                 status: 'Created',
                 time: new Date().toISOString(),
             },
             {
-                actor: 'Barney',
+                actor_id: '1290312938',
+                actor_name: 'Barney', // this would be a dispensary employee filling orders
                 status: 'Filled',
                 time: new Date().toISOString(),
             },
             {
-                actor: 'Bekkie',
+                actor_id: '1290312938',
+                actor_name: 'Bekkie', // this would be the dispensary manager assigning orders
                 status: 'Queued',
                 time: new Date().toISOString(),
             },
             {
-                actor: 'Billy',
+                actor_id: '1239213',
+                actor_name: 'James Ontiveros', // delivery driver
                 status: 'Out For Delivery',
                 time: new Date().toISOString(),
             },
             {
-                actor: null,
+                actor_id: '1239213',
+                actor_name: 'James Ontiveros',
                 status: 'Delivered',
                 time: new Date().toISOString(),
             },
         ],
     },
-    {
+    1238124751: {
         id: '1238124751',
         customer: 'Bob Saget',
         items: 2,
@@ -51,13 +59,20 @@ const INITIAL_STATE = [
         address_zip: '90210',
         statusList: [
             {
-                actor: 'Bob Saget',
+                actor_id: '1238124751',
+                actor_name: 'Bob Saget',
                 status: 'Created',
+                time: new Date().toISOString(),
+            },
+            {
+                actor_id: '1290312938',
+                actor_name: 'Barney', // this would be a dispensary employee filling orders
+                status: 'Filled',
                 time: new Date().toISOString(),
             },
         ],
     },
-    {
+    1238124756: {
         id: '1238124756',
         customer: 'Tony Hawk',
         items: 1,
@@ -69,13 +84,20 @@ const INITIAL_STATE = [
         address_zip: '81091',
         statusList: [
             {
-                actor: 'Tony Hawk',
+                actor_id: '1238124756',
+                actor_name: 'Tony Hawk',
                 status: 'Created',
+                time: new Date().toISOString(),
+            },
+            {
+                actor_id: '1290312938',
+                actor_name: 'Barney', // this would be a dispensary employee filling orders
+                status: 'Filled',
                 time: new Date().toISOString(),
             },
         ],
     },
-    {
+    1238124757: {
         id: '1238124757',
         customer: 'T Pain',
         items: 9,
@@ -87,16 +109,33 @@ const INITIAL_STATE = [
         address_zip: '81920',
         statusList: [
             {
-                actor: 'T Pain',
+                actor_id: '1238124757',
+                actor_name: 'T Pain',
                 status: 'Created',
+                time: new Date().toISOString(),
+            },
+            {
+                actor_id: '1290312938',
+                actor_name: 'Barney', // this would be a dispensary employee filling orders
+                status: 'Filled',
                 time: new Date().toISOString(),
             },
         ],
     }
-];
+};
 
 const orders = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case STATUS_QUEUE:
+            state[action.order_id].statusList.push({
+                actor_id: '1290312938',
+                actor_name: 'Bekkie', // this would be the dispensary manager assigning orders
+                status: 'Queued',
+                time: new Date().toISOString(),
+            })
+            return {
+                ...state,
+            }
         default:
             return state;
     }
