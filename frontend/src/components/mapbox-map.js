01ba854dbb474836fb90c9e2b31b3629c54d560e
Clark <rclark010@gmail.com>
2019-09-06 16:47:48 -0600

    Adds markers

--- frontend/src/components/mapbox-map.js
+++ frontend/src/components/mapbox-map.js
@@ -1,6 +1,12 @@
 import { LitElement, css, html } from 'lit-element';
 
+// import marker from './marker-15.svg';
+
 window.customElements.define('mapbox-map', class extends LitElement {
+	constructor() {
+		super();
+		this._maps = 'hello world';
+	}
 
 	static get styles() {
 		return [
@@ -22,7 +28,6 @@ window.customElements.define('mapbox-map', class extends LitElement {
 	};
 
 	render() {
-		console.log('render', this.shadowRoot.querySelector('div'))
 		return html`
 			<head><link href='./src/components/mapbox-gl.css' rel='stylesheet' /></head>
 			<div id="map"></div>
@@ -40,6 +45,13 @@ window.customElements.define('mapbox-map', class extends LitElement {
 	      style: 'mapbox://styles/mapbox/streets-v10',
 	      zoom: 13,
 	    })
+
+	    // console.log('map', this._map, this);
+
+	    this.loadIcons();
+	    // this._map.on('load', function() {
+	    	// console.log('hello!', () => this._map, this);
+	    // })
     }, 3);
   };
 
@@ -47,4 +59,28 @@ window.customElements.define('mapbox-map', class extends LitElement {
   	console.log('Mapbox Map disconnected');
     this._map.remove()
   };
+
+  loadIcons() {
+  	const map = this._map;
+
+  	/*
+		 * Markers below are just mock ups.
+		 * In production, you'd need the coordinates of each address
+		 * Getting those coordinates is simple with the Mapbox API
+		 * Then, save the coords in the Redux store
+		 * Loop through the coords and add markers for each one
+  	 */
+
+  	var markerOne = new mapboxgl.Marker()
+		  .setLngLat([-105.2705, 40.0150])
+		  .addTo(map);
+
+	  var markerTwo = new mapboxgl.Marker()
+		  .setLngLat([-105.2805, 40.0250])
+		  .addTo(map);
+
+		var markerThree = new mapboxgl.Marker()
+		  .setLngLat([-105.2605, 40.0175])
+		  .addTo(map);
+  }
 });
