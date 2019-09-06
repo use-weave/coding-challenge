26a6212d21b9a0cd2ad9ffce355e90f9cf4c9de9
Clark <rclark010@gmail.com>
2019-09-06 14:22:40 -0600

    Adds grid layout

--- frontend/src/components/weave-assign-orders.js
+++ frontend/src/components/weave-assign-orders.js
@@ -2,7 +2,7 @@ import { LitElement, css, html } from 'lit-element';
 
 import { SharedStyles } from './shared-styles.js';
 
-// import './mapbox-map.js';
+import './mapbox-map.js';
 import './weave-driver-list.js';
 import './weave-order-list.js';
 
@@ -10,17 +10,33 @@ window.customElements.define('weave-assign-orders', class extends LitElement {
 	static get styles() {
 		return [
 			css`
-				:host {
-					margin: 0 auto;
-					max-width: 600px;
-				}
+				/* Wide layout: when the viewport width is bigger than 460px, layout
+        	 changes to a wide layout */
+        @media (min-width: 460px) {
+        	:host {
+        		height: 100%;
+        	}
+					.grid-wrapper {
+						display: grid;
+						grid-template-areas: "driver-list map"
+																 "order-list  map";
+						grid-template-columns: 30rem 1fr;
+						grid-template-rows: 1fr 1fr;
+						height: 100%;
+					}
+
+        }
 			`
 		]
 	}
+
 	render() {
 		return html`
-			<weave-driver-list></weave-driver-list>
-			<weave-order-list></weave-order-list>
+			<div class="grid-wrapper">
+				<weave-driver-list></weave-driver-list>
+				<weave-order-list></weave-order-list>
+				<mapbox-map></mapbox-map>
+			</div>
 		`
 	}
 })
